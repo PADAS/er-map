aad4023da89814021daa13279dd7aa8a7f03578a
ManasiShah1 <shahm5@cs.washington.edu>
2021-01-30 15:46:19 -0800

    render 3D terrain in sattelite view

--- testER/src/App.jsx
+++ testER/src/App.jsx
@@ -1,26 +1,51 @@
-import { hot } from 'react-hot-loader'
 import React from 'react'
 import './App.css'
-import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl'
+import ReactMapboxGl, { MapContext } from 'react-mapbox-gl'
 import 'mapbox-gl/dist/mapbox-gl.css'
 
 const Map = ReactMapboxGl({
   accessToken:
-    'pk.eyJ1Ijoic2hhaG01IiwiYSI6ImNrOXJzNTdndTBkODEzbW10bmdrcWJqc2EifQ.x3zzrFN1gck-G0Jn3Iw2dA'
+    'pk.eyJ1IjoidmpvZWxtIiwiYSI6ImNra2hiZXNpMzA1bTcybnA3OXlycnN2ZjcifQ.gH6Nls61WTMVutUH57jMJQ' // development token
 })
 
-const App = () => (
-  <Map
-    style='mapbox://styles/mapbox/streets-v9'
-    containerStyle={{
-      height: '100vh',
-      width: '100vw'
-    }}
-  >
-    <Layer type='symbol' id='marker' layout={{ 'icon-image': 'marker-15' }}>
-      <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
-    </Layer>
-  </Map>
-)
+const App = () => {
+  return (
+    <div>
+      <Map
+        style='mapbox://styles/mapbox/satellite-v9'
+        containerStyle={{
+          height: '100vh',
+          width: '100vw'
+        }}
+      >
+        <MapContext.Consumer>
+          {(map) => {
+            // add the 3D terrain source
+            map.addSource('mapbox-dem', {
+              type: 'raster-dem',
+              url: 'mapbox://mapbox.mapbox-terrain-dem-v1',
+              tileSize: 512,
+              maxzoom: 14
+            })
+            // add a sky layer that will show when the map is highly pitched
+            map.addLayer({
+              id: 'sky',
+              type: 'sky',
+              paint: {
+                'sky-type': 'atmosphere',
+                'sky-atmosphere-sun': [0.0, 0.0],
+                'sky-atmosphere-sun-intensity': 15
+              }
+            })
+            map.on('render', function () {
+              // add the DEM source as a terrain layer with exaggerated height
+              map.setTerrain({ source: 'mapbox-dem', exaggeration: 3 })
+            })
+          }}
+        </MapContext.Consumer>
+      </Map>
+    </div>
+  )
+}
 
-export default hot(module)(App)
+export default App
