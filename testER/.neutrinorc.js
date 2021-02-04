6984fbaace2eeab0f93e9bc85fba604ed1f27826
ManasiShah1 <shahm5@cs.washington.edu>
2021-02-03 18:23:29 -0800

    run on port 3000

--- testER/.neutrinorc.js
+++ testER/.neutrinorc.js
@@ -1,5 +1,6 @@
 const standard = require('@neutrinojs/standardjs');
 const react = require('@neutrinojs/react');
+const devServer = require('@neutrinojs/dev-server');
 
 module.exports = {
   options: {
@@ -11,6 +12,19 @@ module.exports = {
       html: {
         title: 'testER'
       }
+    }),
+    devServer({
+      port: 3000,
+      hot: true,
+      // Redirect 404s to index.html, so that apps that use the HTML 5 History API work.
+      historyApiFallback: true,
+      // Only display compile duration and errors/warnings, to reduce noise when rebuilding.
+      stats: {
+        all: false,
+        errors: true,
+        timings: true,
+        warnings: true,
+      },
     }),
   ],
 };
