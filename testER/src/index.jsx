80190b87742ea717a85cf593f134f35ce7ebddb9
Chris Jones <chrisj@vulcan.com>
2021-03-13 14:16:37 -0800

    add some constants

--- testER/src/index.jsx
+++ testER/src/index.jsx
@@ -2,4 +2,11 @@ import React from 'react'
 import { render } from 'react-dom'
 import App from './App'
 
+export const {
+    REACT_APP_ER_HOST,
+    REACT_APP_MAPBOX_TOKEN,
+    REACT_APP_ER_API_URL,
+    REACT_APP_ROUTE_PREFIX,
+  } = process.env;
+
 render(<App configFile='./public/config/config.json' />, document.getElementById('root'))
